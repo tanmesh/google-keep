8f2e920497bdafcd8bf8a70be70ed892ec1efc32
Tanmesh <tanmeshnm@gmail.com>
2023-12-29 11:03:58 -0800

    first commit from backend

--- backend-google-keep/src/main/java/org/keep/Main.java
+++ backend-google-keep/src/main/java/org/keep/Main.java
@@ -2,6 +2,6 @@ package org.keep;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Google keep first app backend!");
     }
 }
